4fccb2ae699498449a7a7faf12388ee87f9ab012
a0608390 <kernel.zhou@gmoneycloud.com>
2019-08-04 14:37:06 +0800

    Update write_influxdb.py
    update host

--- write_influxdb.py
+++ write_influxdb.py
@@ -3,11 +3,11 @@
 
 from influxdb import InfluxDBClient
 
-influxdb_ip='172.16.0.80'
+influxdb_ip='172.16.1.108'
 influxdb_port=8086
 influxdb_username='root'
 influxdb_password='Aruba123!'
-influxdb_db_name='example'
+influxdb_db_name='AirWave'
 
 def write_data_db(data):
     client = InfluxDBClient(influxdb_ip, influxdb_port, influxdb_username, influxdb_password,influxdb_db_name)
